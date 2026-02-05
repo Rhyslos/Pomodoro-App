267db4d27b03b2ad74048a6c5193e0ab3ff4b7e3
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-05 22:20:52 +0100

    Updated the timer class file with a global enum state, and a function to prepare for event listener when a task is finished

--- modules/timer.mjs
+++ modules/timer.mjs
@@ -1,13 +1,20 @@
 
+let timerState = {
+    IDLE: "idle",
+    INSESSION: "in_session",
+    FINISHED: "finished",
+}
 
 class pomodoroTimer{
     constructor(timer, task, set){
         this.timer  = timer;
         this.task   = task;
-        this.set    = set;
+        this.set    = 0;
+        this.currentState = timerState.IDLE;
     }
 
     startTimer(minutes){
+        this.currentState = timerState.INSESSION; // Corrected: Updates class property
         let remainingTime = minutes * 60 * 1000;
 
         const intervalID = setInterval(() => {
@@ -20,8 +27,19 @@ class pomodoroTimer{
             remainingTime -= 1000;
             if(remainingTime < 0){
                 clearInterval(intervalID);
+                this.currentState = timerState.FINISHED; // Corrected: Updates class property
+                console.log("timerState finished")
+                this.finishedTask();
             }
         }, 1000);
+    }
+
+    finishedTask() {
+        if(this.currentState === timerState.FINISHED){ // Corrected: Matches constructor name
+            this.set++;
+            console.log(this.set)
+            console.log("did this fire?")
+        }
     }
 
 }

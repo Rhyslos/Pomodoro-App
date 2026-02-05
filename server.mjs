8346fa1cb12f1c0921f2855e45a4b31c85b54377
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-05 20:55:58 +0100

    Pomodoro Timer Test

--- server.mjs
+++ server.mjs
@@ -1,14 +1,21 @@
 import express from 'express';
-import apiRoutes from './api/api.mjs'; 
+//import apiRoutes from './api/api.mjs'; 
+import { pomodoroTimer } from './modules/timer.mjs';
 
 const app = express();
 const PORT = 8080;
+const print = console.log;
 
 app.use(express.json());
 app.use(express.static('Public'));
 app.use('/modules', express.static('Modules'));
-app.use('/api', apiRoutes);
+//app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+const session = new pomodoroTimer("Timer", "test", 1);
+
+print("Starting Pomodoro Session");
+session.startTimer(25);

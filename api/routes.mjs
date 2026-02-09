f213a803368e296c9586082a0d27c3278b271b52
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-09 10:54:35 +0100

    router.post to constrain usernames less than 3 characters

--- api/routes.mjs
+++ api/routes.mjs
@@ -4,3 +4,9 @@ import { createNewUser } from '../modules/user.mjs'
 const router = express.Router();
 const user   = new user();
 
+router.post('/users', (req, res) => {
+    const desiredUsername = req.body.username;
+    if(!desiredUsername || desiredUsername.length < 3) {
+        return res.status(400).json({error: "The username is too short"});
+    }
+} )

d59f04ff89ecdc2e1823e069171e4a6728539587
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-09 21:52:33 +0100

    Updated the class to take handlePhaseChange to switch between states, updated the enum, updated runInterval  and added getStatus

--- modules/timer.mjs
+++ modules/timer.mjs
@@ -1,44 +1,91 @@
-
 let timerState = {
-    IDLE:       "idle",
-    INSESSION:  "in_session",
-    PAUSE:      "pause",
-    FINISHED:   "finished",
+    IDLE: "idle",
+    WORK: "work",
+    BREAK: "break",
+    FINISHED: "finished",
 }
 
-class pomodoroTimer{
-    constructor(timer, task, set){
-        this.timer  = timer;
-        this.task   = task;
-
-        this.targetSets = set;
-        this.set    = 0;
-
-        this.remainingTime = this.timer * 60 * 1000;
+class pomodoroTimer {
+    constructor(workTime, breakTime, task, targetSets) {
+        this.workTime = workTime;
+        this.breakTime = breakTime;
+        this.task = task;
+        this.targetSets = targetSets;
+        
+        this.currentSet = 0;
+        this.remainingTime = this.workTime * 60 * 1000; 
         this.currentState = timerState.IDLE;
-
+        
         this.intervalID = null;
     }
 
-    startTimer(minutes){
-        if(this.currentState === timerState.INSESSION || this.currentState === timerState.PAUSE) return;
-        
-        this.currentState = timerState.INSESSION;
+    startTimer() {
+        if (this.currentState === timerState.WORK || this.currentState === timerState.BREAK) return;
 
-        if(this.remainingTime <= 0 || this.remainingTime === this.timer * 60 * 1000){
-            this.remainingTime = this.timer * 60 * 1000;
+        if (this.currentState === timerState.IDLE) {
+            this.currentState = timerState.WORK;
+            this.remainingTime = this.workTime * 60 * 1000;
         }
 
-        this.runInternval();
+        this.runInterval();
     }
 
-    finishedTask() {
-        if(this.currentState === timerState.FINISHED){
-            this.set++;
-            console.log(this.set)
+    stopTimer() {
+        if (this.intervalID) {
+            clearInterval(this.intervalID);
+            this.intervalID = null;
         }
     }
 
+    runInterval() {
+        if (this.intervalID) clearInterval(this.intervalID);
+
+        this.intervalID = setInterval(() => {
+            const mins = Math.floor(this.remainingTime / 60000);
+            const secs = Math.floor((this.remainingTime % 60000) / 1000);
+            console.log(`Set ${this.currentSet} / ${this.targetSets} finished.`);
+
+            this.remainingTime -= 1000;
+
+            if (this.remainingTime < 0) {
+                clearInterval(this.intervalID);
+                this.handlePhaseChange();
+            }
+        }, 1000);
+    }
+
+    handlePhaseChange() {
+        if (this.currentState === timerState.WORK) {
+            this.currentSet++;
+            console.log(`Set ${this.currentSet} / ${this.targetSets} finished.`);
+
+            if (this.currentSet < this.targetSets) {
+                console.log(`Starting ${this.breakTime} min break...`);
+                this.currentState = timerState.BREAK;
+                this.remainingTime = this.breakTime * 60 * 1000; 
+                this.runInterval(); 
+            } else {
+                this.currentState = timerState.FINISHED;
+                console.log("All sets finished!");
+            }
+        } 
+        else if (this.currentState === timerState.BREAK) {
+            console.log("Break over! Back to work.");
+            this.currentState = timerState.WORK;
+            this.remainingTime = this.workTime * 60 * 1000;
+            this.runInterval(); 
+        }
+    }
+
+    getStatus() {
+        return {
+            state: this.currentState,
+            remaining: Math.ceil(this.remainingTime / 1000),
+            currentSet: this.currentSet,
+            targetSets: this.targetSets,
+            task: this.task
+        };
+    }
 }
 
-export {pomodoroTimer};
+export { pomodoroTimer };

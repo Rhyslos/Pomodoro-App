54d648645176adfc6ee2e307ca60d43e5df4f806
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-09 10:51:04 +0100

    updated user class for adding info to DB

--- modules/user.mjs
+++ modules/user.mjs
@@ -1,9 +1,16 @@
 import { GenerateUserID } from "./lib.mjs";
+import database from "../database/database.sql"
 
 class user{
     
-    createNewUser(username, email, password){
+    createNewUser(username){
         const newUserID = GenerateUserID();
+
+        try{
+            const stmt = database.prepare('INSERT INTO users (user_id, username) VALUES (?, ?)');
+            stmt.run(newID, username);
+            return{id: newID, username: username}
+        }
     }
 
 }

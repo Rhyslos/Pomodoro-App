147691240224ac68ef99932a36099fd5913930db
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-10 18:13:34 +0100

    updated app mjs to create instances of the managers

--- public/app.mjs
+++ public/app.mjs
@@ -1,1 +1,9 @@
+import { UserManager } from './modules/userManager.mjs';
+import { SessionManager } from './modules/sessionManager.mjs';
 
+// Initialization Functions
+const userManager = new UserManager();
+const sessionManager = new SessionManager();
+
+// Export Functions
+export { userManager, sessionManager };

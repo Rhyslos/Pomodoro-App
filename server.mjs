347505b7cafbf734187c6189dcf6680340c71e9c
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-10 18:31:06 +0100

    updated server to take API calls

--- server.mjs
+++ server.mjs
@@ -1,6 +1,5 @@
 import express from 'express';
-//import apiRoutes from './api/api.mjs'; 
-import { pomodoroTimer } from './modules/timer.mjs';
+import apiRoutes from './api/routes.mjs';
 
 const app = express();
 const PORT = 8080;
@@ -8,13 +7,9 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.static('Public'));
 app.use('/modules', express.static('Modules'));
-//app.use('/api', apiRoutes);
+
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
-
-const session = new pomodoroTimer("Timer", "test", 1);
-
-print("Starting Pomodoro Session");
-session.startTimer(0.1);

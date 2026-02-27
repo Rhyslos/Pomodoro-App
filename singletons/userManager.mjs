103eb54433fc6e22c7f7877c3dc6cf4534e145bc
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-27 15:21:13 +0100

    added active users

--- singletons/userManager.mjs
+++ singletons/userManager.mjs
@@ -2,6 +2,9 @@ import { createUserID, createFriendCode } from '../modules/lib.mjs';
 
 // User manager class
 class UserManager {
+    constructor() {
+        this.activeUsers = new Map();
+    }
     
     // User creation functions
     async createUser(username) {
@@ -15,12 +18,23 @@ class UserManager {
             createdAt: new Date().toISOString()
         };
 
+        this.activeUsers.set(userId, candidateUser);
         return candidateUser;
+    }
+
+    async restoreUser(userData) {
+        this.activeUsers.set(userData.userId, userData);
+        return userData;
     }
 
     // Data retrieval functions
     async getUser(userId) {
-        return null; 
+        return this.activeUsers.get(userId) || null; 
+    }
+
+    // User deletion functions
+    async deleteUser(userId) {
+        this.activeUsers.delete(userId);
     }
 }
 

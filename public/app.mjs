96267fa54d5f37a42b3d0c60041304f0ef670962
Yuna Emiya <yuna-emiya@outlook.com>
2026-03-24 13:32:37 +0100

    Troubleshooting service worker

--- public/app.mjs
+++ public/app.mjs
@@ -41,9 +41,15 @@ async function initApp() {
 // service worker functions
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js');
-        });
+        navigator.serviceWorker.register('/sw.js')
+            .then((registration) => {
+                console.log('Service Worker registration successful with scope: ', registration.scope);
+            })
+            .catch((error) => {
+                console.error('Service Worker registration failed: ', error);
+            });
+    } else {
+        console.warn('Service workers are not supported in this browser.');
     }
 }
 
